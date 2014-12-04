f6135a74996d9133387778df7b053cede61cd2d4
Alex Fallah <fallaha@wit.edu>
2014-12-04 15:11:27 -0500

    Updated iteration 2 , Added SVG to ttf convertor(unable to compile) and a semi complete SVG generator

---
+++ Iteration2/src/CloudConvertOpenSource/CloudConvertService.java
@@ -0,0 +1,69 @@
+package org.aioobe.cloudconvert;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.GenericType;
+
+public class CloudConvertService {
+    
+    private final static String API_ROOT = "https://api.cloudconvert.org";
+    
+    String apiKey;
+    WebTarget apiRoot;
+    //Generates an api key specified for your computer
+    //Cloud convert only allows a certain number of conversions based on your api
+    public CloudConvertService(String apiKey) {
+        this.apiKey = apiKey;
+        apiRoot = ClientUtil.createClient().target(API_ROOT);
+    }
+    
+    
+    public ConvertProcess startProcess(String inputFormat, String outputFormat) throws URISyntaxException {
+        ProcessArguments args = new ProcessArguments(apiKey, inputFormat, outputFormat);
+        ProcessResponse response = apiRoot.path("process")
+                                          .request(APPLICATION_JSON)
+                                          .post(Entity.entity(args, APPLICATION_JSON),
+                                                ProcessResponse.class);
+        
+        return new ConvertProcess(response.url, args);
+    }
+    
+    
+    public List<ProcessEntry> listRunningProcesses() {
+        return apiRoot.path("processes")
+                      .queryParam("apikey", apiKey)
+                      .request(APPLICATION_JSON)
+                      .get(new GenericType<List<ProcessEntry>>() {});
+    }
+    
+    
+    @Override
+    public String toString() {
+        return String.format("%s[apiKey: %s]", getClass().getSimpleName(), apiKey);
+    }
+
+
+    public void download(URI url, File dest) throws IOException {
+        InputStream is = new BufferedInputStream(download(url));
+        
+        FileOutputStream fos = new FileOutputStream(dest);
+        
+        int read;
+        byte[] buf = new byte[16 * 1024];
+        while ((read = is.read(buf)) != -1)
+            fos.write(buf, 0, read);
+        
+        fos.close();
+        is.close();
+    }
+    
+    public InputStream download(URI url) {
+    	//returns the file
+        return ClientBuilder.newClient().target(url).request().get(InputStream.class);
+    }
+}
---
+++ Iteration2/src/ConvertProcess.java
@@ -0,0 +1,77 @@
+package org.aioobe.cloudconvert;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import java.io.*;
+import java.net.URI;
+import java.text.ParseException;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.media.multipart.*;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition.FormDataContentDispositionBuilder;
+import org.glassfish.jersey.media.multipart.file.*;
+
+public class ConvertProcess {
+    
+    WebTarget root;
+    ProcessArguments args;
+    
+    public ConvertProcess(URI root) {
+        this(root, null);
+    }
+    
+    
+    public ConvertProcess(URI root, ProcessArguments args) {
+        this.root = ClientUtil.createClient().target(root);
+        this.args = args;
+    }
+    
+    
+    public ProcessStatus getStatus() {
+        return root.request(APPLICATION_JSON)
+                   .get(ProcessStatus.class);
+    }
+    
+    
+    public void startConversion(File file) throws ParseException, FileNotFoundException {
+        if (!file.exists())
+            throw new FileNotFoundException("File not found: " + file);
+        startConversion(new FileDataBodyPart("file", file));
+    }
+    
+    
+    public void startConversion(InputStream input, String filename) throws ParseException {
+        StreamDataBodyPart filePart = new StreamDataBodyPart("file", input);
+        FormDataContentDispositionBuilder builder = FormDataContentDisposition.name("file")
+                                                                              .fileName(filename);
+        filePart.setFormDataContentDisposition(builder.build());
+        startConversion(filePart);
+    }
+    
+    
+    private void startConversion(BodyPart bodyPart) {
+        
+        if (args == null)
+            throw new IllegalStateException("No conversion arguments set.");
+        
+        MultiPart multipart = new FormDataMultiPart().field("input", "upload")
+                                                     .field("outputformat", args.outputformat)
+                                                     .bodyPart(bodyPart);
+        
+        root.request(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(multipart, multipart.getMediaType()));
+    }
+    
+    
+    public void delete() {
+        root.path("delete").request().get();
+    }
+    
+    
+    public void cancel() {
+        root.path("cancel").request().get();
+    }
+    
+}
